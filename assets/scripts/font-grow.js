5d68b90ff86ceb2eb5a4e531e906874a3e0fdb96
Gabriel Castro <gcastro@icasei.com.br>
2020-01-09 11:03:47 -0300

    refactor(js): better usage of js

--- assets/scripts/font-grow.js
+++ assets/scripts/font-grow.js
@@ -2,7 +2,9 @@ document.addEventListener('scroll', event => {
   var text = document.querySelector("#dusseldot");
 
   var topScroll = window.pageYOffset - document.documentElement.clientTop;
-  var topScrollN = (topScroll - 0) * (150 - 50) / (918 - 0) + 50;
+  var topScrollN = map(topScroll, 0, 918, 50, 150);
 
   text.style.fontVariationSettings = `'wdth' ${topScrollN}`;
 });
+
+const map = (value, inMin, inMax, outMin, outMax) => (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
