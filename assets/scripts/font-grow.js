697e1477f94bc21f890a8c75f13bba20281ffd5a
Gabriel Castro <gcastro@icasei.com.br>
2020-01-09 10:43:39 -0300

    Working

--- assets/scripts/font-grow.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', event => {
-  var text = document.querySelector("#dusseldot");
-
-  var topScroll = window.pageYOffset - document.documentElement.clientTop;
-  var topScrollN = (topScroll - 0) * (150 - 50) / (918 - 0) + 50;
-
-  text.style.fontVariationSettings = `'wdth' ${topScrollN}`;
-});
